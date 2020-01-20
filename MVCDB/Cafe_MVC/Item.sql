4e05f777e60c8baafc212eef3c4c9aa1e6b13a64
WillMT <20064082@learner.hkuspace.hku.hk>
2020-01-20 20:36:26 +0800

    1

--- MVCDB/Cafe_MVC/Item.sql
+++ MVCDB/Cafe_MVC/Item.sql
@@ -1,11 +1,12 @@
 ﻿CREATE TABLE [dbo].[Item]
 (
 	[itemID] INT IDENTITY (1,1) NOT NULL,
-	[itemType] NVARCHAR(10) NOT NULL,
-	[itemName] NVARCHAR(50) NOT NULL,
+	[itemType] NVARCHAR(20) NOT NULL,
+	[itemName] NVARCHAR(100) NOT NULL,
 	[iDesc] NVARCHAR (300) NOT NULL,
 	[iStatus] char  null,
+	[iPrice] int not null,
 	[qty] int NOT NULL,
-	[img] varbinary(MAX),
+	[imgdata] varbinary(MAX),
 	PRIMARY KEY CLUSTERED ([itemID] ASC)
 )
