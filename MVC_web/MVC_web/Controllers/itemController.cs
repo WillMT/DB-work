bccef514d13118f534d6a9a539b4e7b0956a50c5
WillMT <20064082@learner.hkuspace.hku.hk>
2020-02-08 20:20:32 +0800

    Add view

---
+++ MVC_web/MVC_web/Controllers/itemController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_web.Controllers
+{
+    public class itemController : Controller
+    {
+        // GET: item
+        public ActionResult Index()
+        {
+            List<Models.DB.Item> itemList = new List<Models.DB.Item>();
+            using (Models.DB.MVCEntities db = new Models.DB.MVCEntities())
+            {
+                itemList = (from s in db.Item select s).ToList();
+                return View(itemList);
+
+            }
+        }
+    }
+}
