269ce93bf11f6d75e5d69bafbd05f3e7e3b355bd
WillMT <20064082@learner.hkuspace.hku.hk>
2020-02-16 00:29:55 +0800

    remove unnessary file

--- MVCDB/Cafe_MVC/Customer.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Customer]
-(
-	[cusID] int IDENTITY (1,1) NOT NULL,
-	[cName] NVARCHAR (50) not NULL,
-	[cphone] int null,
-	[cType] NVARCHAR(15) DEFAULT 'customer',
-	primary key clustered ([cusID] ASC) 
-)
--- MVCDB/Cafe_MVC/Order.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Order]
-(
-	[orderID] INT IDENTITY (1,1) NOT NULL,
-	[CusName] NVARCHAR(50) not null,
-	[orderStatus] NVARCHAR (10) NULL,
-	[OrderTotal] DECIMAL NULL, 
-    [OrderTimeStamp] TIMESTAMP NULL, 
-    PRIMARY KEY CLUSTERED ([orderID] ASC),
-)
